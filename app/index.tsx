9c60c5d4864e571a6cd0f817d5f6a5adbd842202
KatieMari <katie.glover@student.norwichuni.ac.uk>
2026-04-21 11:02:20 +0100

    set up fun facts

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import BackHomeCard from "@/components/BackHomeCard";
+import Header from "@/components/Header";
 
-export default function Index() {
+import ScreenBackground from "@/components/ScreenBackground";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+
+export default function InstructionsScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Our App</Text>
-    </View>
+
+    <ScreenBackground>
+      <View style={styles.page}>
+        <View>
+
+          <Header
+            title="Fun Facts"
+            leftIconName="arrow-circle-left"
+            rightIconName="cog"
+            onBackPress={() => console.log("Back Pressed")}
+            onSettingsPress={() => console.log("Settings Pressed")}
+          />
+
+          <View style={styles.spacer12} />
+
+          
+
+          <View style={styles.spacer12} />
+
+          
+          <View style={styles.spacer12} />
+
+          
+
+          <View style={styles.spacer12} />
+
+          
+
+          <View style={styles.spacer12} />
+
+         =
+
+          <View style={styles.spacer20} />
+
+        </View>
+
+        <BackHomeCard
+          title="Back Home"
+          onBackHomePress={() => console.log("Back Home Pressed")}
+        />
+      </View>
+
+    </ScreenBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  spacer12: {
+    height: 12,
+  },
+  spacer20: {
+    height: 20,
+  },
+});
