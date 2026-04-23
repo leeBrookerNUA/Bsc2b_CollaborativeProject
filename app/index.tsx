c13cb6c030323b0cb32fb3e61e0a890dd3758637
Alfie135 <182797639+Alfie135@users.noreply.github.com>
2026-04-23 19:25:23 +0100

    tidying
    wanted to make things a bit neater

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/Header';
-import React from 'react';
+import Header from "../components/Header";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import AppText from "../components/AppText";
 import ButtonCard from "../components/ButtonCard";
