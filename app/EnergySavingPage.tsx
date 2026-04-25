2b4d1f3e49063c5483f545d1d67245d2721a5852
KatieMari <katie.glover@student.norwichuni.ac.uk>
2026-04-25 16:29:25 +0100

    name changed

--- app/EnergySavingPage.tsx
+++ app/EnergySavingPage.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 
-export default function InstructionsScreen() {
+export default function EnergySavingPage() {
   return (
 
     <ScreenBackground>
