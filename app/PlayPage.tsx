92fd0133c9e7e488234db3fb80753931aca37e49
KatieMari <katie.glover@student.norwichuni.ac.uk>
2026-04-25 16:08:59 +0100

    renamed index

--- app/PlayPage.tsx
+++ app/PlayPage.tsx
@@ -9,7 +9,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 
-export default function PlayScreen() {
+export default function PlayPage() {
   return (
     <ScreenBackground>
 
