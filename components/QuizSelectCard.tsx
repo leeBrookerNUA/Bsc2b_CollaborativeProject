22f537f5ddcd4ab0c5705e8699d913dc8951f0cb
KatieMari <katie.glover@student.norwichuni.ac.uk>
2026-04-26 11:42:35 +0100

    quiz select page done

--- components/QuizSelectCard.tsx
+++ components/QuizSelectCard.tsx
@@ -1,28 +1,28 @@
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { ColorValue, StyleSheet, View } from "react-native";
+import { ColorValue, Pressable, StyleSheet, View } from "react-native";
 import AppHeading from "./AppHeading";
 import AppText from "./AppText";
 
-interface FactCardProps {
-  iconName: React.ComponentProps<typeof FontAwesome5>["name"];
+interface QuizSelectCardProps {
+  iconName: React.ComponentProps<typeof AntDesign>["name"];
   title: string;
-  subText: string;
+  subTitle: string,
   gradientColors?: readonly [ColorValue, ColorValue, ...ColorValue[]];
+  onQuizButtonPress?: () => void;
 }
 
-export default function FactCard(props: FactCardProps) {
+export default function QuizSelectCard(props: QuizSelectCardProps) {
   const {
-    iconName,
-    title,
-    subText,
-    gradientColors = ["#FFF2A6", "#FFE066", "#E0C120"],
-  } = props;
+    iconName, title, subTitle, gradientColors = ["#FFF2A6", "#FFE066", "#E0C120"], onQuizButtonPress, } = props;
 
   return (
     <View style={styles.shadowWrapper}>
-      <View style={styles.card}>
+      <Pressable
+        onPress={onQuizButtonPress}
+        style={({ pressed }) => [styles.card, pressed && styles.cardPressed]}
+      >
         <LinearGradient
           colors={gradientColors}
           style={styles.gradient}
@@ -33,13 +33,13 @@ export default function FactCard(props: FactCardProps) {
 
         <View style={styles.content}>
           <View style={styles.iconContainer}>
-            <FontAwesome5 name={iconName} size={36} color="#FFFFFF" />
+            <AntDesign name={iconName} size={32} color="#FFFFFF" />
           </View>
 
           <AppHeading style={styles.title}>{title}</AppHeading>
-          <AppText style={styles.subText}>{subText}</AppText>
+          <AppText style={styles.subTitle}>{subTitle}</AppText>
         </View>
-      </View>
+      </Pressable>
     </View>
   );
 }
@@ -47,7 +47,7 @@ export default function FactCard(props: FactCardProps) {
 const styles = StyleSheet.create({
   shadowWrapper: {
     width: "100%",
-    borderRadius: 30,
+    borderRadius: 24,
 
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
@@ -59,42 +59,47 @@ const styles = StyleSheet.create({
   card: {
     position: "relative",
     width: "100%",
-    minHeight: 220,
+    height: 140,
     borderRadius: 24,
     overflow: "hidden",
     justifyContent: "center",
     alignItems: "center",
   },
 
+  cardPressed: {
+    transform: [{ scale: 0.98 }],
+    opacity: 0.95,
+  },
+
   gradient: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
   },
 
   darkOverlay: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(0, 0, 0, 0.1)",
   },
 
   lightOverlay: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(255, 255, 255, 0.05)",
   },
 
- content: {
-  zIndex: 1,
-  width: "100%",
-  paddingTop: 10,
-  paddingBottom: 22,
-  paddingHorizontal: 20,
-  alignItems: "center",
-  justifyContent: "center",
-},
+  content: {
+    zIndex: 1,
+    width: "100%",
+    height: "100%",
+    paddingVertical: 12,
+    paddingHorizontal: 14,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
   iconContainer: {
     backgroundColor: "rgba(255,255,255,0.1)",
     borderRadius: 99,
-    width: 60,
-    height: 60,
+    width: 52,
+    height: 52,
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 6,
@@ -103,11 +108,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     textAlign: "center",
-    marginBottom: 10,
   },
-
-  subText: {
-    fontSize: 20,
+   subTitle: {
+    fontSize: 16,
     textAlign: "center",
     lineHeight: 20,
   },
